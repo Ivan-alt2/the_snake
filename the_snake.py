dc570c635537faeede44dc83fb058cbaf07979cf
Ivan-alt2 <2011bor6@gmail.com>
2026-03-22 20:44:52 +0900

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -3,7 +3,7 @@ import pygame
 from random import randint, choice
 
 
-# Константы для размеров поля и сетки:
+# Константы для размеров поля и сетки
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
@@ -12,13 +12,13 @@ GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 # Центр экрана
 CENTER_OF_SCREEN = (SCREEN_HEIGHT // 2, SCREEN_WIDTH // 2)
 
-# Направления движения:
+# Направления движения
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-# Цвет фона - черный:
+# Цвет фона - черный
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 
 # Цвет границы ячейки
