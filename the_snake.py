2aebef4782285435357db9530b92e10a8a8a6cd4
Ivan-alt2 <2011bor6@gmail.com>
2026-03-22 21:25:43 +0900

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -2,8 +2,7 @@ from random import randint, choice
 
 import pygame
 
-
-# Константы для размеров поля и сетки
+# Константы для размеров поля и сетки:
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
@@ -12,13 +11,13 @@ GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 # Центр экрана
 CENTER_OF_SCREEN = (SCREEN_HEIGHT // 2, SCREEN_WIDTH // 2)
 
-# Направления движения
+# Направления движения:
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-# Цвет фона - черный
+# Цвет фона - черный:
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 
 # Цвет границы ячейки
@@ -33,16 +32,16 @@ LEAF_COLOR = (0, 128, 0)
 # Цвет змейки
 SNAKE_COLOR = (0, 255, 0)
 
-# Скорость движения змейки
+# Скорость движения змейки:
 SPEED = 20
 
-# Настройка игрового окна
+# Настройка игрового окна:
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
-# Заголовок окна игрового поля
+# Заголовок окна игрового поля:
 pygame.display.set_caption('Змейка')
 
-# Настройка времени
+# Настройка времени:
 clock = pygame.time.Clock()
 
 
